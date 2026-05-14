7729c1c77bff4206084d92080d9c9854de598333
chase <forlack@gmail.com>
2026-05-14 14:57:24 -0700

    Sync local fish/kitty/tmux tweaks
    - fish: fastfetch greeter (skipped inside tmux), mp/greet abbrs
    - kitty: 0.5-step font-size shortcuts, disable audio bell
    - tmux: new windows open in $HOME

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,17 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+# Only show greeter outside of tmux (i.e., fresh kitty sessions)
+function fish_greeting
+    if not set -q TMUX
+        fastfetch
+    end
+end
 
 # opencode
 fish_add_path /home/chase/.opencode/bin
 
-alias mp='mpremote'
+abbr -a mp mpremote
+abbr -a greet fastfetch
 
 # --- Tmux Auto-Naming ---
 # Automatically rename tmux window to current folder name
